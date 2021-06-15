f3648a738be446627ec6965ab91635ca0c1e86e4
MoSaber <msaberms72016@gmail.com>
2021-06-15 15:57:04 +0200

    added the Pool Generic class and fixed Concurrency pbs

--- Pages/ObjectBoolPattern_Example.xcplaygroundpage/Contents.swift
+++ Pages/ObjectBoolPattern_Example.xcplaygroundpage/Contents.swift
@@ -1,2 +1,53 @@
 import Foundation
 
+class Book {
+    let author: String
+    let title: String
+    let stockNumber: Int
+    var reader: String?
+    var checkoutCount = 0
+    
+    init(author: String, title: String, stockNumber: Int) {
+        self.author = author
+        self.stockNumber = stockNumber
+        self.title = title
+    }
+}
+
+
+class Pool<T> {
+    private var data = [T]()
+    private let serialQ = DispatchQueue(label: "Saber.serial.queue")
+    let semaphore: DispatchSemaphore
+    
+    init(items: [T]) {
+        data.reserveCapacity(items.count)
+        semaphore = DispatchSemaphore(value: data.count)
+        for item in items {
+            data.append(item)
+        }
+    }
+    
+    func getFromPool() ->T?{
+        semaphore.wait()
+        serialQ.sync {
+            return self.data[0]
+
+            }
+    return nil
+    
+}
+    func returnToPool(item: T){
+        serialQ.async {
+            self.data.append(item)
+            self.semaphore.signal()
+        }
+    }
+}
+
+
+class Library {
+    private var books: [Book]
+    private var pool : Pool<Book>
+    
+}
